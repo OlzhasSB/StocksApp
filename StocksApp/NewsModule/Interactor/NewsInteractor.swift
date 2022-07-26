b0c69f8f7694bcabf6a407edd4cde9531ea21d2c
moldaly <aida.moldaly.2000@mail.ru>
2022-07-26 18:17:25 +0600

    Pulling from Olzhik Entity

--- StocksApp/NewsModule/Interactor/NewsInteractor.swift
+++ StocksApp/NewsModule/Interactor/NewsInteractor.swift
@@ -29,7 +29,7 @@ final class NewsInteractor: NewsInteractorInput {
     
     func obtainNews() {
         network.loadNews(path: "/api/v1/news") { [weak self] news in
-//            self?.output.didLoadNews(news)
+            self?.output.didLoadNews(news)
         }
     }
     
