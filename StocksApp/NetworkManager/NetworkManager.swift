491f1e1531077930106035aded052dcad80e6217
moldaly <aida.moldaly.2000@mail.ru>
2022-07-26 14:59:17 +0600

    Added NeworkManager

---
+++ StocksApp/NetworkManager/NetworkManager.swift
@@ -0,0 +1,67 @@
+//
+//  NetworkManager.swift
+//  StocksApp
+//
+//  Created by Aida Moldaly on 26.07.2022.
+//
+
+import Foundation
+
+final class NetworkManager {
+    
+    static var shared = NetworkManager()
+    
+    var urlComponents: URLComponents = {
+        var components = URLComponents()
+        components.scheme = "https"
+        components.host = "aidateam.herokuapp.com"
+        return components
+    }()
+
+    private let session: URLSession
+
+    private init() {
+        session = URLSession(configuration: .default)
+    }
+    
+    func loadMentorProfile(completion: @escaping (News) -> Void) {
+        
+        var components = urlComponents
+        components.path = "/api/user/profile"
+
+        guard let url = components.url else {
+            return
+        }
+        
+        var urlRequest = URLRequest(url: url)
+        urlRequest.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        urlRequest.addValue("application/json", forHTTPHeaderField: "Accept")
+        urlRequest.httpMethod = "GET"
+        
+        let task = session.dataTask(with: urlRequest) { data, response, error in
+            guard error == nil else {
+                print("Error: error calling GET")
+                return
+            }
+            guard let data = data else {
+                print("Error: Did not receive data")
+                return
+            }
+            guard let response = response as? HTTPURLResponse, (200 ..< 300) ~= response.statusCode else {
+                print("\(String(describing: response))")
+                print("Error: HTTP request failed")
+                return
+            }
+            do {
+                let news = try JSONDecoder().decode(News.self, from: data)
+                DispatchQueue.main.async {
+                    completion(news)
+                }
+
+            } catch {
+                print("no json")
+            }
+        }
+        task.resume()
+    }
+}
